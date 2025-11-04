b48a7f29234781af6301159e7cc1a5b6cb506b15
Serdar <181284499+serdarvarl@users.noreply.github.com>
2025-11-04 21:39:05 +0100

    update db
    update db php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 function getBD() {
     $host = '127.0.0.1';
-    $port = '8889';     // MAMP portu
-    $db   = 'miel';     // veritabanı adın
+    $port = '8889';     // MAMP port
+    $db   = 'miel';     // nom DV
     $user = 'root';
     $pass = 'root';
 
-    // PDO kullanımı (TP3'ün önerdiği gibi)
+    // PDO TP3
     try {
         $bdd = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
