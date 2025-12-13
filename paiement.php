49989c5a604869d5fea93b8a1108fe27af97ee85
Serdar <181284499+serdarvarl@users.noreply.github.com>
2025-12-13 22:05:06 +0100

    update payment
    payment ok

--- paiement.php
+++ paiement.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['client']) || !isset($_SESSION['panier']) || count($_SESSIO
 }
 
 // 1. Stripe test sk
-\Stripe\Stripe::setApiKey('sk_test_51SQoxfD95YOAhUESBHuNKueEujx8JHvFmRKMdhQUk8oBLoEKByvPUEjKsHgduoCMlKVK2My9lGaHEBrxB1XlIcAU00V3rkj9kT');
+\Stripe\Stripe::setApiKey('***************************');
 
 // 2. je calcule somme de panier pour ça recuperer les prix sur db
 $pdo = getBD();
